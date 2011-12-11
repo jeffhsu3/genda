325f33b35b4437089b1bd44b1019adef21d84f70
kai <ki2278@roadrunner.com>
2011-12-11 17:58:16 -0500

    Uses pysam.fetch to grab alleles

--- scripts/AEI/allele_counter.py
+++ scripts/AEI/allele_counter.py
@@ -31,17 +31,13 @@ Written by Jeffrey Hsu
 """
 # Standard Libraries
 import optparse, pickle
-import cProfile
 
 import pysam
 import numpy as np
 
-from pySeq.formats.VCF import VCFfile
-import pySeq.stats.likelihood_funcs as lf
-
-
-"""
 class AlleleCounter():
+    """ Gets the allele counts 
+    """
     BASE_INDEX = {'A':0, 'a':0, 'C':1, 'c':1, 'G':2, 'g':2, 'T':3, 't':3}
     def __init__(self, region, position, phredThreshold=0):
         self.region = region
@@ -71,70 +67,12 @@ class AlleleCounter():
                 index = position - alignment.pos - 1   
             if index >= 0:
                 base = alignment.seq[index]
-                base =  self.BASE_INDEX[base]
                 b_qual = alignment.qual[index]
                 if base != "N" and ord(b_qual)-33 > qualT:
+                    base =  self.BASE_INDEX[base]
                     self.counts[base] += 1
                 else: pass
             else: pass
-            """
-
-
-
-class lociInformation():
-    """ Contains information about a given position in a BAM file
-    """
-
-    BASE_INDEX = {'A':0, 'a':0, 'C':1, 'c':1, 'G':2, 'g':2, 'T':3, 't':3}
-    def __init__(self, region, position, phredThreshold=0):
-        """ Location is one-based.  Internally this will get changed to zero
-        base by pysam.
-        """
-        self.region = region
-        self.position = int(position)
-        # So allele_counts['sample'] = [0,14,0,15]
-        # BASE_INDEX converts index to base, ie C, in 14
-        self.phredThreshold = phredThreshold
-        self.counts = np.zeros(4, dtype=np.uint32)
-
-    def __call__(self, pileups, position=None,
-        phredThreshold=None, genotype=None):
-        """ Genotype file contains the infromation the actualy genotyping
-        calls.
-
-        """
-        if position == None: position = self.position
-        else:  pass
-        if phredThreshold == None: qualT = self.phredThreshold
-        if pileups.pos != position-1: pass
-        else:
-            for read in pileups.pileups:
-                if read.alignment.is_duplicate or read.alignment.mapq <= 50:
-                    pass
-                else:
-                    qpos = read.qpos
-                    base_quality = read.alignment.qual[qpos]
-                    if ord(base_quality)-33 > qualT:
-                        base = read.alignment.seq[qpos]
-                        base = self.BASE_INDEX[base]
-                        #strand = read.alignment.is_reverse
-                        self.counts[base] += 1
-                    else: pass
-
-
-
-def threshold_counts(counts, threshold=30, number=1):
-    """ Returns True
-
-    Makes sure that at least one of the samples meet a read count threshold.
-
-    """
-    counter = 0
-    for i in counts:
-        if sum(i) > threshold: counter += 1
-    if counter >= number:return True
-    else: return False
-
 
 def main():
     """ Main loop that iterates over the desired loci to calculate Aellic
@@ -150,22 +88,18 @@ def main():
     p = optparse.OptionParser(__doc__)
     p.add_option("-o", "--output", dest="filename", help="write \
             report to FILE")
+    p.add_option("-I", "--input", dest="input", 
+            help="BAM files are listed in a file instead of command line\
+                    arguments")
     p.add_option("-G", "--genotype", dest="G", help=\
             "Use imputed/genotypes if available, should be in VCF file format")
     p.add_option("-v",  "--vcf_file", action="store_true", dest="inputisvcfile",
                  help="the input is a VCF file")
     p.add_option("-q", "--quality_threshold", type="int", dest="qual",
                  help="base quality threshold to take allele counts from")
-    p.add_option("-p", "--pileup", action="store_true", dest="p",
-                 help= "Input files are pileup files")
     p.add_option("-D", "--debug", action="store_true", dest="D", help="debug")
-    p.add_option("-c", "--count-threshold", action="store", type="int",
-                 dest="c", help="Set the count threshold for making AEI calls")
     p.add_option("-V", "--output_vcf", action="store_true", dest="outputVCF",
                  help="Output the results to a VCF file")
-    p.add_option("-A", "--auto_parse", action="store_true", dest="auto",
-                 help="Autoparse readgroups, if set to false will assume a\
-                 single sample in each file")
 
     options, args = p.parse_args()
     if options.qual: pass
@@ -179,21 +113,23 @@ def main():
     file_a = open(args[0],"rb")
 
     # Handling of multiple BAM/SAM inputs
-    bam_Names = args[1:]
+    if not options.input:
+        bam_Names = args[1:]
+    else:
+        bam_Names = []
+        inputs = open(options.input, 'rU')
+        for line in inputs:
+            bam_Names.append(line.strip("\n"))
+
     bam_files = []
     for filename in bam_Names:
         bam_files.append(pysam.Samfile(filename,"rb"))
 
 
     INDEX_BASE = ['A', 'C', 'G', 'T']
-    if options.c:
-        count_threshold = options.c
-    else:
-        count_threshold = 20
-
     counts_matrix = []
     c_m = []
-    rsID = {}
+    rsID = []
     t = 0
     for line in file_a:
         counts = []
@@ -211,22 +147,22 @@ def main():
             if position - start != 1:
                 isIndel = True
             else: 
-                rsID[line[-1]] = t 
+                rsID.append(line[-1])  
                 t += 1
                 isIndel = False
         
         if not isIndel:
             for bamfile, bamNames in map(None, bam_files, bam_Names):
                 # :TODO in the VCF and bed files make sure to type the attributes
-                variant = lociInformation(region, position,
-                                          phredThreshold=options.qual)
-                bamfile.pileup(variant.region, variant.position,
-                               variant.position+1, callback=variant)
-                
+                p_v = AlleleCounter(region, position,
+                        phredThreshold=options.qual)
 
-                counts.append(variant.counts)
+                bamfile.fetch(p_v.region, p_v.position-1, p_v.position-1,
+                        callback=p_v)
 
-            counts_matrix.append(counts)
+                c.append(p_v.counts)
+
+            c_m.append(c)
         else: 
             # For right now
             pass
@@ -237,12 +173,13 @@ def main():
             else: debug += 1
         else:pass
     
+    INDEX_BASE = ['A', 'C', 'G', 'T']
     out = {}
-    out['counts'] = counts_matrix
+    out['counts'] = c_m
     out['rsIDs'] = rsID
     pickle.dump(out, output)
     #counts_df = pd.DataFrame(counts_matrix)
     #counts_df.save(opttion.out)
 
 if __name__ == '__main__':
-    cProfile.run('main()')
+    main()
