1e9ea5a55f74332470a62d84d008379b044dec90
Jeffrey Hsu <jeffhsu3@gmail.com>
2014-04-17 18:00:03 -0400

    More fixes

--- genda/pysam_callbacks/allele_counter.pyx
+++ genda/pysam_callbacks/allele_counter.pyx
@@ -1,37 +1,61 @@
 import numpy as np
-
 cimport numpy as np
 
 from cpython cimport bool
-
+#from multiprocessing import Process
+#import cython
 cimport pysam.csamtools as csam
-from pysam.csamtools cimport AlignedRead
-from cython.parallel cimport prange
+from pysam.csamtools cimport AlignedRead, bam1_t, bam_pileup1_t
+from libc.stdlib cimport malloc, free
+from cpython cimport PyObject
 #cimport csamtools
 DTYPE = np.uint16
-
 ctypedef np.uint16_t DTYPE_t
+ctypedef np.float64_t dtype_t 
 
 cdef inline int int_max(int i , int j): return i if i >= j else j
 
+'''
+cpdef object construct_known_var(csam.Samfile samfile, PileupProxy col):
+    """ Counts the various alleles at a given position.
+    """
 
-class AlleleCounter(object):
+    cdef int i # loop index
+'''
+
+cdef class AlleleCounter:
     """ Counts the various alleles at a given position.
 
     usage:
     import pysam
-    t = AlleleCounter("chr1", snp-position - 1)
-
-    bamfile.fetch("chr1", 124415155, 124415156)
+    t = AlleleCounter(c_m, "chr1", snp-position - 1)
+    bamfile.fetch("chr1", 124415155, 124415156, callback=t)
     t.counts = [A counts, C counts, G counts, T counts]
+
+    :TODO refactor out callback
     """
-    def __init__(self, region, int position, int phredThreshold=0, bool isIndel = False):
-        cdef dict BASE_INDEX
-        self.BASE_INDEX = {'A':0, 'a':0, 'C':1, 'c':1, 'G':2, 'g':2, 'T':3, 't':3}
+    #cdef DTYPE_t [:] counts
+    def __cinit__(self, char *region, 
+            int position, int phredThreshold=0, bool isIndel = False):
+        #cdef dict BASE_INDEX
+        #:TODO get rid of this dictionary make it in C?
+        #self.BASE_INDEX = {'A':0, 'a':0, 'C':1, 'c':1, 'G':2, 'g':2, 'T':3, 't':3}
+        #cdef np.ndarray counts = np.zeros(4, dtype=DTYPE_t)
+        cdef int A_n = 0
+        cdef int G_n = 0
+        cdef int C_n = 0
+        cdef int T_n = 0
+        self.A_n = A_n
+        self.G_n = G_n
+        self.C_n = C_n
+        self.T_n = T_n
         self.region = region
         self.position = int(position)
         self.phredThreshold = phredThreshold
-        self.counts = np.zeros(4, dtype=np.uint16)
+        print('hi_c')
+        #self.counts = np.zeros(4, dtype=np.uint16)
+        
+
 
     def  __call__(self, AlignedRead alignment, int position = 0,
                  int phredThreshold = 0,
@@ -40,15 +64,16 @@ class AlleleCounter(object):
         cdef int inserts
         cdef char *b_qual
         cdef char *base
-        cdef int iindex
+        #cdef int iindex
         cdef int i_s
         cdef int i_e
+        cdef char *base_pair
         #cdef np.ndarray[np.uint16, ndim=1] counts = self.counts
         #cdef bool alignment.is_duplicaate
-
         # Settting up variables
         position = self.position
         #qualT = self.phredThreshold
+        #print(alignment.seq)
         
         #if alignment.is_duplicate:
         if False:
@@ -57,6 +82,7 @@ class AlleleCounter(object):
             inserts = 0
             i_e = alignment.pos
             i_s = alignment.pos
+
             for i in alignment.cigar:
                 # 0 is cigar string match
                 inserts += i[0] * 1/3 * i[1]
@@ -65,37 +91,32 @@ class AlleleCounter(object):
                     break
                 elif i_s <= position < i_e:
                     index = position - inserts - alignment.pos - 1 
+                    # :TODO INDELS!
+                    base_pair = alignment.seq[index]
+                    if base_pair == b'A' or base_pair == b'a':
+                        self.A_n += 1
+                    elif base_pair == b'C' or base_pair == b'c':
+                        self.C_n += 1
+                    elif base_pair == b'G' or base_pair == b'g':
+                        self.G_n += 1
+                    elif base_pair == b'T' or base_pair == b't':
+                        self.T_n += 1
+                    else:
+                        pass
+                    break
+                    """
                     try:
+                        
                         iindex = self.BASE_INDEX[alignment.seq[index]]
                         self.counts[iindex] += 1
                         break
+                    except KeyError:
+                        pass
                     except IndexError:
                         pass
+                    """
                 else:
                     pass
                 i_s = i_e
-
-
-'''
-cpdef np.ndarray[DTYPE_t, DTYPE_t] aei_counts_samples(np.ndarray[DTYPE_t, ndim=2] c_m, bool reduced = False, 
-        chrom = None, pos=None, path= None, bamfile_func = column_names_to_bams):
-    """ A parallel cython version, should make this part of a class
-    """
-    cdef int n_samples
-    n_samples = c_m.shape[0]
-    sample_genotype.index
-    for i in prange(n_samples, nogil=True):
-        bamfile = pysam.Samfile(sample_genotype.name, 'rb')
-        print(sample_genotype.name)
-        for i, j, k in zip(chrom, pos, index):
-            variant = AlleleCounter(str(i), j, phredThreshold=0)
-            bamfile.fetch(variant.region, variant.position, variant.position +
-                    1, callback=variant)
-        with gil:
-            c_m.ix[k, i * 4: i * 4 + 3] = variant.counts.T
-
-    return(c_m)
-'''
-
 
 
