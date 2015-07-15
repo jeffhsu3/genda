33ce010ef63b0106a2675d0e5e7c9536a10cb089
jeffhsu3 <jeffhsu3@gmail.com>
2015-07-14 21:37:47 -0400

    removed some redundent scripts

--- genda/formats/PED.py
+++ genda/formats/PED.py
@@ -1,3 +1,5 @@
+"""
+"""
 import Genotype
 import pandas as pd
 import numpy as np
@@ -5,12 +7,15 @@ import numpy as np
 class PED(Genotype.Genotype):
 
     def __init__(self, PED, MAP, encoder = None):
-        self.PED = pd.read_table(PED, delim_whitespace = True, header = None)
-        self.MAP = pd.read_table(MAP, delim_whitespace = True, header = None)
+        self.PED = pd.read_table(PED, 
+                delim_whitespace = True, header = None)
+        self.MAP = pd.read_table(MAP, 
+                delim_whitespace = True, header = None)
         if encoder != None:
             self.geno = self.genotype(self.PED, self.MAP, encoder)
         else:
             self.geno = None
+
 
     def genotype(self, PED, MAP, encoder):
         geno = PED.ix[:, 6:]
@@ -19,13 +24,17 @@ class PED(Genotype.Genotype):
             snps.extend([s]*2)
         geno.columns = snps
         geno = geno.apply(self.apply_encoder, encoder = encoder)
-        geno = pd.DataFrame([[geno.ix[r,c*2] + geno.ix[r,(c*2)+1]\
-                for c in range(geno.shape[1]/2)] for r in range(geno.shape[0])])
+        geno = pd.DataFrame([[geno.ix[r,c*2] +\
+            geno.ix[r,(c*2)+1]\
+            for c in range(geno.shape[1]/2)] for r\
+            in range(geno.shape[0])])
+
         geno = geno.transpose()
         geno.index = MAP.ix[:,1]
         geno.columns = PED.ix[:,1]
         return geno
         
+
     def apply_encoder(self, PED, encoder):
         snp = encoder[PED.name]
         encoding_dict = {snp[0] : 0, snp[2] : 1, '0': np.nan}
